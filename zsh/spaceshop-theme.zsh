2f8bf879a774e0768bd43d85087eb5878ec05316
Yatimisi <yatimisi2012@gmail.com>
2019-12-27 19:03:12 +0800

    Midify config from files

--- zsh/spaceshop-theme.zsh
+++ zsh/spaceshop-theme.zsh
@@ -18,5 +18,7 @@ SPACESHIP_GIT_PREFIX='git'
 SPACESHIP_EXIT_CODE_SHOW='true'
 SPACESHIP_TIME_SHOW='true'
 SPACESHIP_EXEC_TIME_ELAPSED='0'
+SPACESHIP_EXEC_TIME_PREFIX=''
+SPACESHIP_TIME_COLOR=''
 SPACESHIP_RPROMPT_ORDER=(rprompt_prefix exit_code time exec_time rprompt_suffix)
 SPACESHIP_PROMPT_ORDER=(user host dir git line_sep venv char)
