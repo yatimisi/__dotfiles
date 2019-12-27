eee304d50154bc4cc08212c0eb1c0946df4a1ca5
Yatimisi <yatimisi2012@gmail.com>
2019-12-27 18:08:11 +0800

    Add user and host for ssh

--- zshrc.zsh
+++ zshrc.zsh
@@ -23,12 +23,11 @@ SPACESHIP_VENV_GENERIC_NAMES=' '  # empty array not work so use space to let thi
 SPACESHIP_VENV_PREFIX='('
 SPACESHIP_VENV_SUFFIX=') '
 SPACESHIP_VENV_COLOR='magenta'
-SPACESHIP_DIR_PREFIX=' '
 SPACESHIP_DIR_TRUNC='0'
 SPACESHIP_DIR_TRUNC_REPO='false'
 SPACESHIP_GIT_SYMBOL=':'
 SPACESHIP_GIT_PREFIX='git'
-SPACESHIP_PROMPT_ORDER=(dir git line_sep venv char)
+SPACESHIP_PROMPT_ORDER=(user host dir git line_sep venv char)
 antigen theme denysdovhan/spaceship-prompt
 
 ## spaceship theme settings - mode B
