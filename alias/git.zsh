70eeb4946501bba5f0a2cc2459ef1bc12423815f
Yatimisi <yatimisi2012@gmail.com>
2020-02-04 15:16:26 +0800

    update 2020/02/04-15:16:26

--- alias/git.zsh
+++ alias/git.zsh
@@ -12,3 +12,14 @@ alias gc='git commit -a'
 alias gm='git commit -m'
 alias gmi='git commit -m "feat: Initial commit"'
 alias gz='git cz'
+_gac() {
+    git add --all
+    if [ "$1" != "" ]
+    then
+      git commit -m "$1"
+    else
+      DATE=$(date +%Y/%m/%d-%H:%M:%S)
+      git commit -m "update $DATE" # default commit message is `update`
+    fi
+}
+alias gac=_gac;
