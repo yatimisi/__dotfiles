bff950df5519290965dd92bf1bdd6ef6e701b82d
Yatimisi <yatimisi2012@gmail.com>
2020-04-07 11:27:47 +0800

    Update 0407

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@ set -e
 
 command -v git > /dev/null || { echo "Git not installed"; exit 1; }
 
+if [ ! -d "$HOME/.antigen" ]; then
+    git clone https://github.com/zsh-users/antigen.git $HOME/.antigen
+fi
+
 if [ ! -d "$HOME/.dotfiles" ]; then
     git clone https://github.com/yatimisi/dotfiles.git $HOME/.dotfiles
 fi
@@ -11,22 +15,18 @@ if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-cd $HOME/.dotfiles
-
-git submodule init
-git submodule update
-
 if [ ! -d "$HOME/.pip" ]; then
     mkdir $HOME/.pip
 fi
 
-ln -si $HOME/.dotfiles/zsh $HOME/.zsh
 ln -si $HOME/.dotfiles/zshrc.zsh $HOME/.zshrc
 ln -si $HOME/.dotfiles/gitconfig $HOME/.gitconfig
+ln -si $HOME/.dotfiles/pypirc $HOME/.pypirc
 ln -si $HOME/.dotfiles/pip.conf $HOME/.pip/pip.conf
 ln -si $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
-ln -si $HOME/.dotfiles/pypirc $HOME/.pypirc
 ln -si $HOME/.dotfiles/czrc $HOME/.czrc
+
+cp $HOME/.dotfiles/gitconfig.user $HOME/.gitconfig.user
 
 if [ $(uname) != "Darwin" ]; then  # No conky for OSX
     if [ -d "$HOME/.local/share/applications" ]; then  # No conky for no GUI
