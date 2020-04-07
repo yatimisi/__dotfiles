9ff60ff55646a5aaabc3e2c67bd76e566d78377e
Yatimisi <yatimisi2012@gmail.com>
2020-04-07 12:05:04 +0800

    Update

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,5 +1,5 @@
 ## Load antigen
-source $HOME/.antigen/bin/antigen.zsh
+source "$HOME/.antigen/bin/antigen.zsh"
 
 ## Load the oh-my-zsh's library.
 antigen use oh-my-zsh
@@ -9,12 +9,13 @@ antigen bundle pip
 antigen bundle docker
 antigen bundle docker-compose
 antigen bundle web-search
+antigen bundle qoomon/zsh-lazyload
 antigen bundle zsh-users/zsh-completions
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-syntax-highlighting
 
 ## Load theme config
-source $HOME/.zsh/theme.zsh
+source "$HOME/.dotfiles/zsh/theme.zsh"
 
 ## Set theme
 antigen theme denysdovhan/spaceship-prompt
@@ -23,7 +24,10 @@ antigen theme denysdovhan/spaceship-prompt
 antigen apply
 
 ## Load custom settings
-source $HOME/.zsh/alias.zsh
-source $HOME/.zsh/note.zsh
+source "$HOME/.dotfiles/zsh/alias.zsh"
+source "$HOME/.dotfiles/zsh/note.zsh"
+source "$HOME/.dotfiles/zsh/config.zsh"
 
-source $HOME/.zsh/environmental.zsh
+for f in $HOME/.dotfiles/zsh/environmentals/*.zsh; do source $f; done
+
+lazyload pyenv python pip pipenv poetry -- 'source "$HOME/.dotfiles/zsh/environmentals/lazyload/python.zsh"'
